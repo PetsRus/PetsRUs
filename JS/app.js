1f99fee4c67b825b20c499a6acc235a0bfa68cd9
Jon Gitter <jonathan.m.gitter@gmail.com>
2022-03-01 15:20:42 -0700

    new image names, js code

--- JS/app.js
+++ JS/app.js
@@ -1,50 +1,72 @@
-'use strict'; 
+'use strict';
 
 let allAnimals = [];
 
 //constructor for all animals 
 function Animal (name, species, age, interest){
-  this.name = name, 
-  this.species = species, 
-  this.age = age, 
-  this.interest = interest, 
-  this.src = `../imgs/${name}.jpg`
+  this.name = name,
+  this.species = species,
+  this.age = age,
+  this.interest = interest,
+  this.src = `../imgs/${name}.jpg`,
   //should the image be stored in this.src? or is there a better way to store an image? 
-  allAnimals.push(this); 
+  allAnimals.push(this);
 }
-
+// dog instantiations
 let tucker = new Animal('tucker', 'dog', 3, 'hunting');
+let luanne = new Animal('luanne', 'dog', 5, 'walks');
+let ramona = new Animal('ramona', 'dog', 8, 'swimming');
+let sonja = new Animal('sonja', 'dog', 4, 'tug of war');
+let tinsly = new Animal('tinsly', 'dog', 10, 'fetch');
+let carol = new Animal('carol', 'dog', 7, 'rawhides');
+let dorinda = new Animal('dorinda', 'dog', 6, 'drama');
+
+// bird instantiations
 let chirpy = new Animal('chirpy', 'bird', 2, 'singing');
+let jerry = new Animal('jerry', 'bird', 4, 'comedy');
+let elaine = new Animal('elaine', 'bird', 6, 'whistling');
+let george = new Animal('george', 'bird', 10, 'eating');
+let kramer = new Animal('kramer', 'bird', 12, 'flying');
+let newman = new Animal('newman', 'bird', 14, 'pecking');
+let jackie = new Animal('jackie', 'bird', 40, 'talking');
+
+// cat instantiations
 let luke = new Animal('luke','cat', 4, 'lasers');
+let mace = new Animal('mace','cat', 7, 'feather toys');
+let anakin = new Animal('anakin','cat', 9, 'mice');
+let obi = new Animal('obi','cat', 11, 'jazz music');
+let leia = new Animal('leia','cat', 8, 'scratching posts');
+let jarjar = new Animal('jarjar','cat', 5, 'catnip');
+let han = new Animal('han','cat', 10, 'zoomies');
 
 
 
 // console.log(allAnimals); 
-// let imgWindow = document.getElementById('img-one'); 
+// let imgWindow = document.getElementById('img-one');
 
 let myContainer = document.getElementById('profiles');
 
 
 function renderTucker (){
-  let img1 = document.createElement('img'); 
-  img1.src = allAnimals[0].src; 
-  let capt1 = document.createElement('figcaption'); 
-  // capt1.textContent = 'hello'; 
+  let img1 = document.createElement('img');
+  img1.src = allAnimals[0].src;
+  let capt1 = document.createElement('figcaption');
+  // capt1.textContent = 'hello';
 
-  let ul = document.createElement('ul'); 
-  let li1 = document.createElement('li'); 
-    li1.textContent = allAnimals[0].name; 
-    ul.appendChild(li1); 
+  let ul = document.createElement('ul');
+  let li1 = document.createElement('li');
+    li1.textContent = allAnimals[0].name;
+    ul.appendChild(li1);
   let li2 = document.createElement('li');
     li2.textContent = allAnimals[0].age;
-    ul.appendChild(li2);  
+    ul.appendChild(li2);
   let li3 = document.createElement('li');
-    li3.textContent = allAnimals[0].interest; 
-    ul.appendChild(li3); 
+    li3.textContent = allAnimals[0].interest;
+    ul.appendChild(li3);
 
-  capt1.appendChild(ul); 
-  myContainer.appendChild(img1); 
-  myContainer.appendChild(capt1); 
+  capt1.appendChild(ul);
+  myContainer.appendChild(img1);
+  myContainer.appendChild(capt1);
 }
 
 renderTucker();
